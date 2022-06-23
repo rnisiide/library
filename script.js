b0230f2aad598163c311a66f56721d21d5d117bf
Ricardo Nisiide <rnisiide@gmail.com>
2022-06-23 19:24:47 -0300

    function to udpdate table completed. next steps are remove button, checkboxes and final CSS

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 console.log('I am sentient')
 
+
 let myLibrary = [
   {author: 'Douglas Adams',
   title: "The ULTIMATE Hitchhiker's Guide to the galaxy",
   pages: 815,
-  read: true  
-}
-];
+  read: true,
+  remove: 0,  
+}];
 
+let headers = [
+  'Author', 'Title', 'Number of pages', 'Have you read it?', 'Remove'
+]
 
 const modal = document.getElementById('myModal');
 const modalSpan = document.getElementById('modalSpan');
@@ -23,21 +27,14 @@ const removeBook = document.querySelector('.remove');
 const newBook = document.querySelector('#newbook');
 const submit = document.querySelector('.submit');
 
-function Book (title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    console.log(title, author, pages, read)
-    this.info = function () {
-        console.log(`${title}, by ${author}, has ${pages} pages. Read: ${read}.`)
-       if (read == true || read == 'yes') {
-        return `${title}, by ${author}, has ${pages} pages. I have read the book.`; 
-        } 
-    if (read == false || read == 'no') {return `${title}, by ${author}, has ${pages} pages. I haven't read the book yet.`;}
-}
-}
 
+function Book (author, title, pages, read, remove) {
+  this.author = author;
+  this.title = title;    
+  this.pages = pages;
+  this.read = read;
+  this.remove = ` Remove ${myLibrary.length}`;
+}
 
 
 newBook.addEventListener('click', () => {
@@ -49,7 +46,48 @@ newBook.addEventListener('click', () => {
 })
 
 submit.addEventListener('click', () => {
-  myLibrary.push(new Book(addTitle.value, addAuthor.value, addPages.value, addHasread.checked));
+  myLibrary.push(new Book(addAuthor.value, addTitle.value, addPages.value, addHasread.checked));
+  
+  updateList();
+  
   console.table(myLibrary);
   modal.style.display = "none";
+  addTitle.value = '';
+  addAuthor.value = '';
+  addPages.value = '';
+  addHasread.checked = false;
 })
+
+const bookTable = document.querySelector('#table')
+
+function updateList() {
+  while (bookTable.firstChild) {
+  bookTable.removeChild(bookTable.firstChild);
+  }
+
+  let headerRow = document.createElement('tr');
+
+  headers.forEach (headerText => {
+    let header = document.createElement('th');
+    let textNode = document.createTextNode(headerText);
+    header.appendChild(textNode);
+    headerRow.appendChild(header);
+  })
+  
+  bookTable.appendChild(headerRow);
+
+
+  myLibrary.forEach(book => {
+    let row = document.createElement('tr');
+    Object.values(book).forEach(text => {
+      let cell = document.createElement('td');
+      let cellText = document.createTextNode(text);
+      cell.classList.add(myLibrary.length - 1);
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+    })
+    
+    bookTable.appendChild(row);
+
+  })
+}
