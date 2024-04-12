1b17b8ea91433854813fa4b54cbf43504795bfe2
Ricardo Nisiide <rnisiide@gmail.com>
2024-04-11 22:00:55 -0300

    second part of the assignment. converted function constructor to class declaration. fixed bugs

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 console.log('I am sentient')
 
+const container = document.getElementById('books-cards-container');
+const newBook = document.querySelector('#newbook');
+
+//modal selectors
+const modal = document.getElementById('myModal');
+const modalSpan = document.getElementById('modalSpan');
+const addAuthor = document.querySelector('#author');
+const addTitle = document.querySelector('#title');
+const addPages = document.querySelector('#pages');
+const addHasread = document.querySelector('#hasread');
+const submit = document.querySelector('.submit');
+
+
 let myLibrary = [
   {
     author: 'Douglas Adams',
@@ -14,92 +27,87 @@ let myLibrary = [
     read: true,
   }];
 
-
-function returnCard(arr) {
-  innerText = '';
-  for (i = 0; i < arr.length; i++) {
-    if (arr[i].read == true) {
-      innerText += `<div class="books-cards">
-      <div>
-        <div class="book-title">
-          <h4>${arr[i].title}</h4>
-        </div>
-        <div class="book-author">      
-          <p> written by ${arr[i].author}</p>
-        </div>
-        <div class="book-pages">      
-          <p> ${arr[i].pages} pages</p>
-        </div> 
-        <div class="book-read">      
-          <input type="checkbox" name="${arr[i].title}" onclick="changeRead(this.name)" checked>
-          <label class="check-box" id="check-box-${i}" for="${arr[i].title}"> I have read the book</label><br>
-        </div>         
-        <button class="remove-button" value="${arr[i].title}" onclick="removeBook(this.value)"> Remove this book    
-        </button>
-      </div></div>`
-    } else {
-      innerText += `<div class="books-cards">
-      <div>
-        <div class="book-title">
-          <h4>${arr[i].title}</h4>
-        </div>
-        <div class="book-author">      
-          <p> written by ${arr[i].author}</p>
-        </div>
-        <div class="book-pages">      
-          <p> ${arr[i].pages} pages</p>
-        </div> 
-        <div class="book-read">      
-          <input type="checkbox" name="${arr[i].title}" onclick="changeRead(this.name)"  >
-          <label class="check-box" id="check-box-${i}" for="${arr[i].title}"> I haven't read the book</label><br>
-        </div>         
-        <button class="remove-button" value="${arr[i].title}" onclick="removeBook(this.value)"> Remove this book 
-        </button>
-      </div></div>`
-    }
-  } return innerText;
+class Book {
+  constructor(author, title, pages, read) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+  }
 }
-
-function Book(author, title, pages, read) {
-  this.author = author;
-  this.title = title;
-  this.pages = pages;
-  this.read = read;
-}
-
-const container = document.getElementById('books-cards-container');
-
-const modal = document.getElementById('myModal');
-const modalSpan = document.getElementById('modalSpan');
-const addAuthor = document.querySelector('#author');
-const listAuthor = document.querySelector('.list_author');
-const addTitle = document.querySelector('#title');
-const listTitle = document.querySelector('.list_title');
-const addPages = document.querySelector('#pages');
-const listPages = document.querySelector('.list_pages');
-const addHasread = document.querySelector('#hasread');
-const listHasread = document.querySelector('.list_hasread');
-const newBook = document.querySelector('#newbook');
-const submit = document.querySelector('.submit');
-
 
 newBook.addEventListener('click', () => {
-  console.log(`New book works!!`)
   modal.style.display = 'block';
   modalSpan.onclick = function () {
     modal.style.display = "none";
   }
 })
 
-//this will start the cards
-container.innerHTML = returnCard(myLibrary);
+function returnCards(arr) {
+  container.innerHTML = '';
+
+  for (let i = 0; i < arr.length; i++) {
+    let name = arr[i].title
+    const wrapperCard = document.createElement('div');
+    wrapperCard.className = 'books-cards';
+    const card = document.createElement('div');
+    card.setAttribute('id', `card-${i}`)
+    const bookTitle = document.createElement('div');
+    bookTitle.className = 'book-title';
+    const titleH4 = document.createElement('h4');
+    const bookAuthor = document.createElement('div');
+    bookAuthor.className = "book-author";
+    const textP = document.createElement('p');
+    const bookPages = document.createElement('div');
+    bookPages.className = "book-pages";
+    const textPages = document.createElement('p');
+    const bookRead = document.createElement('div');
+    bookRead.className = 'book-read';
+    const checkBox = document.createElement('input');
+    checkBox.setAttribute('type', 'checkbox');
+    checkBox.setAttribute('name', `${arr[i].title}`);
+    checkBox.setAttribute('onclick', "changeRead(this.name)");
+    const checkBoxLabel = document.createElement('label');
+    checkBoxLabel.className = 'check-box';
+    checkBoxLabel.id = `check-box-${i}`;
+    const removeButton = document.createElement('button')
+    removeButton.className = "remove-button";
+    removeButton.setAttribute('value', `${arr[i].title}`);
+    removeButton.setAttribute('onclick', 'removeBook(this.value)');
+    titleH4.innerText = `${arr[i].title}`;
+    textP.innerText = `written by ${arr[i].author}`;
+    removeButton.innerText = `Remove this book`;
+    textPages.innerText = `${arr[i].pages} pages`
+
+    container.appendChild(wrapperCard);
+    wrapperCard.appendChild(card);
+    card.appendChild(bookTitle);
+    bookTitle.appendChild(titleH4);
+    card.appendChild(bookAuthor);
+    bookAuthor.appendChild(textP);
+    card.appendChild(bookPages);
+    bookPages.appendChild(textPages);
+    card.appendChild(bookRead);
+    bookRead.appendChild(checkBox);
+    bookRead.appendChild(checkBoxLabel);
+    if (arr[i].read == true) {
+      checkBoxLabel.innerText = "I have read the book";
+      checkBox.checked = true;
+
+    } else {
+      checkBoxLabel.innerText = "I haven't read the book";
+    }
+
+    card.appendChild(removeButton);
+
+  }
+}
+returnCards(myLibrary);
 
 
 submit.addEventListener('click', () => {
   myLibrary.push(new Book(addAuthor.value.toString(), addTitle.value.toString(), addPages.value, addHasread.checked));
-
-  container.innerHTML = returnCard(myLibrary);
-
+  returnCards(myLibrary);
   console.table(myLibrary);
   modal.style.display = "none";
   addTitle.value = '';
@@ -111,11 +119,10 @@ submit.addEventListener('click', () => {
 function removeBook(i) {
   let index = myLibrary.findIndex(p => p.title == i);
   myLibrary.splice(index, 1)
-  container.innerHTML = returnCard(myLibrary);
+  returnCards(myLibrary);
 }
 
 
-let clicks = 0;
 function changeRead(i) {
   let index = myLibrary.findIndex(p => p.title == i);
   let changeText = document.getElementById('check-box-' + index);
@@ -127,4 +134,6 @@ function changeRead(i) {
     changeText.innerHTML = "I have read the book"
   }
 }
+
+
 
